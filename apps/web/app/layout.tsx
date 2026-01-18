e812cc59fd66a236e1a0e319e49106cf89acf277
tyler <209597026+conjureme@users.noreply.github.com>
2026-01-18 04:11:18 -0600

    style(ui): create sako-light theme using daisyui variables

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' data-theme='sako-light'>
       <body className={geist.className}>{children}</body>
     </html>
   );
